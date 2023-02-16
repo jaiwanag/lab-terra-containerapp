b82806b2a1552da2729fd50467d5f97090df8a2c
Jayson <jiwanaga@hotmail.com>
2023-02-15 21:21:06 -1000

    update labels

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "lab" {
   name     = "lab-rg"
   location = "West US"
 }
 
-resource "azurerm_log_analytics_workspace" "example" {
+resource "azurerm_log_analytics_workspace" "lab" {
   name                = "lab-law"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
@@ -11,13 +11,13 @@ resource "azurerm_log_analytics_workspace" "example" {
   retention_in_days   = 30
 }
 
-resource "azurerm_container_app_environment" "example" {
+resource "azurerm_container_app_environment" "lab" {
   name                       = "lab-cae"
   location                   = azurerm_resource_group.example.location
   resource_group_name        = azurerm_resource_group.example.name
   log_analytics_workspace_id = azurerm_log_analytics_workspace.example.id
 }
-resource "azurerm_container_app" "example" {
+resource "azurerm_container_app" "lab" {
   name                         = "lab-ca"
   container_app_environment_id = azurerm_container_app_environment.example.id
   resource_group_name          = azurerm_resource_group.example.name

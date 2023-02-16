c855a8acf8b31f564389fdf97d3a52fb0e04390e
Jayson <jiwanaga@hotmail.com>
2023-02-15 21:25:29 -1000

    updated resources

--- main.tf
+++ main.tf
@@ -5,22 +5,22 @@ resource "azurerm_resource_group" "lab" {
 
 resource "azurerm_log_analytics_workspace" "lab" {
   name                = "lab-law"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.lab.location
+  resource_group_name = azurerm_resource_group.lab.name
   sku                 = "PerGB2018"
   retention_in_days   = 30
 }
 
 resource "azurerm_container_app_environment" "lab" {
   name                       = "lab-cae"
-  location                   = azurerm_resource_group.example.location
-  resource_group_name        = azurerm_resource_group.example.name
-  log_analytics_workspace_id = azurerm_log_analytics_workspace.example.id
+  location                   = azurerm_resource_group.lab.location
+  resource_group_name        = azurerm_resource_group.lab.name
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.lab.id
 }
 resource "azurerm_container_app" "lab" {
   name                         = "lab-ca"
-  container_app_environment_id = azurerm_container_app_environment.example.id
-  resource_group_name          = azurerm_resource_group.example.name
+  container_app_environment_id = azurerm_container_app_environment.lab.id
+  resource_group_name          = azurerm_resource_group.lab.name
   revision_mode                = "Single"
 
   template {
